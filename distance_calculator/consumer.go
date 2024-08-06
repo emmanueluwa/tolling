9d164c7f8f72cf62626ffbeb85ee2ecb7a778c9c
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2024-08-06 16:56:19 +0100

    adding ability for distance calculator to consume data

---
+++ distance_calculator/consumer.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/confluentinc/confluent-kafka-go/kafka"
+    "github.com/sirupsen/logrus"
+)
+
+
+//this could also be called kafka transport
+type KafkaConsumer struct {
+    consumer *kafka.Consumer
+    isRunning bool
+}
+
+
+func NewKafkaConsumer(topic string) (*KafkaConsumer, error) {
+    c, err := kafka.NewConsumer(&kafka.ConfigMap{
+        "bootstrap.servers": "localhost",
+        "group.id": "myGroup",
+        "auto.offset.reset": "earliest",
+    })
+
+    if err != nil {
+        return nil, err
+    }
+
+    c.SubscribeTopics([]string{topic}, nil)
+
+    return &KafkaConsumer{
+        consumer: c,
+    }, nil
+}
+
+
+func (c *KafkaConsumer) Start() {
+    logrus.Info("kafka transport started")
+    c.isRunning = true
+    c.readMessageLoop()
+}
+
+
+func (c *KafkaConsumer) readMessageLoop() {
+    for c.isRunning {
+        msg, err := c.consumer.ReadMessage(-1)
+
+        if err != nil {
+            logrus.Errorf("kafka consume error %s", err)
+            continue
+        }
+        fmt.Println(msg)
+    }
+}
+
---
+++ distance_calculator/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "log"
+)
+
+//type DistanceCalculator struct {
+//    consumer DataConsumer
+//}
+
+
+const kafkaTopic = "obudata"
+
+
+func main() {
+    kafkaConsumer, err := NewKafkaConsumer(kafkaTopic)
+    if err!= nil {
+        log.Fatal(err)
+    }
+
+    kafkaConsumer.Start()
+}
